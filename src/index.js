2a746abbd11e27c2b93c8fb73afa81ea4ae5d0c0
Joe McPartland <joemcpartland@gmail.com>
2021-12-18 12:44:18 -0700

    start 12-18-2021

--- src/index.js
+++ src/index.js
@@ -33,7 +33,6 @@ function democratSenators() {
 const idDemocrats = document.getElementById('democrats-btn');
 idDemocrats.addEventListener('click', event => democratSenators())
 
-
 function republicanSenators() {
   members.innerHTML = ''
   const republicansFilter = allSenators.filter(member => member.party == 'R')
@@ -41,7 +40,6 @@ function republicanSenators() {
 };
 const idRepublicans = document.getElementById('republicans-btn')
 idRepublicans.addEventListener('click', event => republicanSenators())
-
 
 function independentSenators() {
   members.innerHTML = ''
@@ -53,21 +51,21 @@ idIndependents.addEventListener('click', event => independentSenators())
 
 
 function membersList(senators) {
-  for (senator of senators) {
-    const firstLastName = `${senator.first_name} ${senator.last_name}`
-
+  for (s of senators) {
+    const firstLastName = `${s.first_name} ${s.last_name}`
+    
     const headshotImageTag = document.createElement('img')
-    headshotImage(headshotImageTag)
+    renderHeadshot(headshotImageTag)
 
     const cardColumn = document.createElement('div')
     cardColumn.className = 'column'
-
+    
     const card = document.createElement('div')
     card.className = 'card'
     
     const nameTag = document.createElement('p')
     nameTag.textContent = firstLastName
-
+    
     card.appendChild(headshotImageTag)
     card.appendChild(nameTag)
     
@@ -76,15 +74,16 @@ function membersList(senators) {
 };
 
 
-function headshotImage(imageTag) {
-  let headshot = `https://theunitedstates.io/images/congress/225x275/${senator.id}.jpg`
-  let headshotSilhouette = 'https://static.propublica.org/rails/assets/congress/silhouette_male-eb244690d4f7eee39ace3ca9cb41fa44f908b1677ba256fc2522496917d2af02.png'
+function renderHeadshot(imageTag) {
+  const headshot = `https://theunitedstates.io/images/congress/225x275/${s.id}.jpg`
+  const headshotSilhouette = 'https://static.propublica.org/rails/assets/congress/silhouette_male-eb244690d4f7eee39ace3ca9cb41fa44f908b1677ba256fc2522496917d2af02.png'
   
   imageTag.className = 'member-headshot'
   imageTag.addEventListener('mouseenter', (event) => event.target.style.opacity = '50%');
   imageTag.addEventListener('mouseleave', (event) => event.target.style.opacity = '100%');
   imageTag.addEventListener('click', (event) => memberDetails());
   
+
   // Add silhouette if headshot does not exist
   fetch(headshot).then(function(response) {
     if (response.status == 200) {
@@ -93,24 +92,31 @@ function headshotImage(imageTag) {
       imageTag.src = headshotSilhouette
     }
   })
+
 }
 
 
 function memberDetails() {
   members.innerHTML = ''
 
-  console.log(senator)
+  const headshotImageTag = document.createElement('img')
+  renderHeadshot(headshotImageTag)
 
   const detailName = document.createElement('p')
-  detailName.textContent = `Name: ${senator.first_name} ${senator.last_name}`;
+    detailName.textContent = `Name: ${s.first_name} ${s.last_name}`;
   const detailTitle = document.createElement('p')
-  detailTitle.textContent = `Title: ${senator.title}`;
+    detailTitle.textContent = `Title: ${s.title}`;
   const detailParty = document.createElement('p')
-  detailParty.textContent = `Party: ${senator.party}`;
+    detailParty.textContent = `Party: ${s.party}`;
   const detailDob = document.createElement('p')
-  detailDob.textContent = `DOB: ${senator.date_of_birth}`;
+    detailDob.textContent = `DOB: ${s.date_of_birth}`;
   const detailGender = document.createElement('p')
-  detailGender.textContent = `Gender: ${senator.gender}`;
+    detailGender.textContent = `Gender: ${s.gender}`;
 
-  members.append(detailName, detailTitle, detailParty, detailDob, detailGender)
+  const detailCard = document.createElement('div')
+    detailCard.className = 'detailCard'
+
+  detailCard.appendChild(headshotImageTag)
+
+  members.append(detailCard, detailName, detailTitle, detailParty, detailDob, detailGender)
 };
--- src/index2.js
+++ src/index2.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', event => getSenators());
 const senateMembers = 'https://api.propublica.org/congress/v1/117/senate/members.json'
 const propublicaKey = 'tNm5YIP9zO7SCYymYDfjB73IRmhUzMmC8beETVXI'
 
+const members = document.querySelector('.members')
+
 let allSenators = [];
 
 function getSenators() {
@@ -24,45 +26,91 @@ function returnedData(data) {
 
 
 function democratSenators() {
-  const dSenators = allSenators.filter(member => member.party == 'D')
-  namesList(dSenators)
+  members.innerHTML = ''
+  const democratsFilter = allSenators.filter(member => member.party == 'D')
+  membersList(democratsFilter)
 };
-const idDemocrats = document.getElementById('democrats');
-idDemocrats.addEventListener('click', event => democratSenators())
+const idDemocrats = document.getElementById('democrats-btn');
+// idDemocrats.addEventListener('click', event => democratSenators())
 
 
 function republicanSenators() {
-  const rSenators = allSenators.filter(member => member.party == 'R')
-  namesList(rSenators);
+  members.innerHTML = ''
+  const republicansFilter = allSenators.filter(member => member.party == 'R')
+  membersList(republicansFilter);
 };
-const idRepublicans = document.getElementById('republicans')
+const idRepublicans = document.getElementById('republicans-btn')
 idRepublicans.addEventListener('click', event => republicanSenators())
 
 
 function independentSenators() {
-  const iSenators = allSenators.filter(member => member.party == 'ID')
-  namesList(iSenators);
+  members.innerHTML = ''
+  const independentsFilter = allSenators.filter(member => member.party == 'ID')
+  membersList(independentsFilter);
 };
-const idIndependents = document.getElementById('independents')
+const idIndependents = document.getElementById('independents-btn')
 idIndependents.addEventListener('click', event => independentSenators())
 
 
-const memberList = document.querySelector('.memberList')
-const rowCard = document.querySelector('.row')
+function membersList(senators) {
+  for (senator of senators) {
+    const firstLastName = `${senator.first_name} ${senator.last_name}`
 
-
-function namesList(senators) {
-  for (s of senators) {
-    const firstLastName = `${s.first_name} ${s.last_name}`
+    const headshotImageTag = document.createElement('img')
+    headshotImage(headshotImageTag)
 
     const cardColumn = document.createElement('div')
     cardColumn.className = 'column'
 
     const card = document.createElement('div')
     card.className = 'card'
-    card.innerHTML = firstLastName
     
-    cardColumn.appendChild(card)
-    rowCard.appendChild(cardColumn)
+    const nameTag = document.createElement('p')
+    nameTag.textContent = firstLastName
+
+    card.appendChild(headshotImageTag)
+    card.appendChild(nameTag)
+    
+    members.appendChild(card)
   }
+};
+
+
+function headshotImage(imageTag) {
+  let headshot = `https://theunitedstates.io/images/congress/225x275/${senator.id}.jpg`
+  let headshotSilhouette = 'https://static.propublica.org/rails/assets/congress/silhouette_male-eb244690d4f7eee39ace3ca9cb41fa44f908b1677ba256fc2522496917d2af02.png'
+  
+  imageTag.className = 'member-headshot'
+  imageTag.addEventListener('mouseenter', (event) => event.target.style.opacity = '50%');
+  imageTag.addEventListener('mouseleave', (event) => event.target.style.opacity = '100%');
+  imageTag.addEventListener('click', (event) => memberDetails());
+  
+  // Add silhouette if headshot does not exist
+  fetch(headshot).then(function(response) {
+    if (response.status == 200) {
+      imageTag.src = headshot
+    } else {
+      imageTag.src = headshotSilhouette
+    }
+  })
+}
+
+
+function memberDetails() {
+  members.innerHTML = ''
+
+  console.log(senator)
+
+  const detailName = document.createElement('p')
+  detailName.textContent = `Name: ${senator.first_name} ${senator.last_name}`;
+  const detailTitle = document.createElement('p')
+  detailTitle.textContent = `Title: ${senator.title}`;
+  const detailParty = document.createElement('p')
+  detailParty.textContent = `Party: ${senator.party}`;
+  const detailDob = document.createElement('p')
+  detailDob.textContent = `DOB: ${senator.date_of_birth}`;
+  const detailGender = document.createElement('p')
+  detailGender.textContent = `Gender: ${senator.gender}`;
+
+  members.append(detailName, detailTitle, detailParty, detailDob, detailGender)
 };
